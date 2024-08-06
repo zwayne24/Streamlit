a7978c690f851b6021872f83e4df003fd743b277
Zachary Wayne <zachrwayne@gmail.com>
2024-08-06 10:40:27 -0400

    heatmap updates

--- streamlitTest.py
+++ streamlitTest.py
@@ -1,22 +1,19 @@
+# imports
 import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors  
 import seaborn as sns
 import os
 import plotly.express as px
 
+# make page wider margins
 st.set_page_config(layout="wide")
 
+# title
 st.title('First Watch Report')
-
-df = pd.read_excel('./output.xlsx')
-
-blankIndex = df[df.isnull().all(1)].index[0]
-
-
-df1 = pd.read_excel('output.xlsx', skiprows= lambda x: x not in range(blankIndex+1))
 
 # Insert containers separated into tabs:
 tabs = ["Total Revenue", "AOV", "Number of Transactions", "Visits", "Number of Orders by Income"]
@@ -26,14 +23,15 @@ for i in range(len(tabs)):
     with tabslist[i]:
         if i != len(tabs)-1:
             title = tabs[i]
-            # import data output.xslx
+            # read in excel and find blank row, the excel is set up so the data and YoY % for that data are on the same
+            # excel sheet separated by a blank row, so this is to find that blank row and split the data into two dataframes
             df = pd.read_excel('output.xlsx', sheet_name=title)
-
             blankIndex = df[df.isnull().all(1)].index[0]
 
-
+            # read in the two dataframes, using skiprows to get the data above and below the blank row
             df1 = pd.read_excel('output.xlsx', sheet_name=title, skiprows= lambda x: x not in range(blankIndex+1))
             df2 = pd.read_excel('output.xlsx', sheet_name=title, skiprows= lambda x: x not in range(blankIndex+2, df.shape[0]+1))
+            # rename the first column to Brand Name and set it as the index
             df1 = df1.rename(columns={'Unnamed: 1': 'Brand Name'})      
             df1.set_index(['Brand Name'], inplace=True)
             df2 = df2.rename(columns={'Unnamed: 1': 'Brand Name'})
@@ -43,9 +41,10 @@ for i in range(len(tabs)):
             df1 = df1.iloc[:, 1:]
             df2 = df2.iloc[:, 1:]
 
-            # index = first two columns
             to_drop = None
-            # change anything datetime to string including headers 
+            # -Change anything datetime to string, including headers. This is to make the headers more readable
+            # -Because the overall data has more columns than the YoY data and they're read in off the same sheet
+            # we find the first column that has 'Unnamed' in it in the YoY and drop everything after that
             for col in range(len(df1.columns)):
                 if isinstance(df1.columns[col], datetime.date):
                     df1 = df1.rename(columns={df1.columns[col]: df1.columns[col].strftime('%m/%d/%Y')})
@@ -59,24 +58,23 @@ for i in range(len(tabs)):
             # drop columns from to_drop onwards 
             df2 = df2.iloc[:, :to_drop]
             
-            # add blank row to row 2 and move everything down
+            # Add a blank row to separate the data into First Watch, Breakfast, and Casual Dining
             df_blank_bc = pd.DataFrame(np.nan, index=["BREAKFAST COMPS"], columns=df2.columns)
             df_blank_cd = pd.DataFrame(np.nan, index=["CASUAL DINING COMPS"], columns=df2.columns)
             df2_with_blanks = pd.concat([df2.iloc[:1], df_blank_bc, df2.iloc[1:9], df_blank_cd, df2.iloc[9:]])
             
-            styled_df2 = df2_with_blanks.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
+            # Create heatmap of the data, where 0% is white, negative is red, and positive is green
+            divnorm = colors.TwoSlopeNorm(vmin=min(df2.values.min(),-.001), vcenter=0, vmax=max(df2.values.max(),.001))
+            styled_df2 = df2_with_blanks.style.background_gradient(cmap="RdYlGn", axis=None, vmin=0, vmax=1, gmap=df2.apply(divnorm)).format("{:.0%}")
             
-            # create df of just the first row, then the average of rows 2-9, then the average of rows 10-17
+            # Create a df of the averages for First Watch, Breakfast, and Casual Dining
             dfAverages = pd.concat([df2_with_blanks.iloc[0], df2_with_blanks.iloc[2:10].mean(), df2_with_blanks.iloc[12:].mean()], axis=1).T
             # index the averages
             dfAverages.index = ['First Watch', 'Breakfast Average', 'Casual Dining Average']
-            styled_dfAverages = dfAverages.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
-            
-            # remove category column
-            df1 = df1.iloc[:, 1:]
-            df2 = df2.iloc[:, 1:]
+            divnorm = colors.TwoSlopeNorm(vmin=min(dfAverages.values.min(),-.001), vcenter=0, vmax=max(dfAverages.values.max(),.001))
+            styled_dfAverages = dfAverages.style.background_gradient(cmap="RdYlGn", axis=None, vmin=0, vmax=1, gmap=dfAverages.apply(divnorm)).format("{:.0%}")
         
-            # making the charts
+            # making the line charts. First Watch is always black, then breakfast is purples/blues, casual dining is yellows/reds
             fig1 = px.line(df1.T,
                         color_discrete_map={'First Watch': 'black',
                                                 df1.index[1]: '#260279',
@@ -96,7 +94,7 @@ for i in range(len(tabs)):
                                                 df1.index[15]: '#ce3b00',
                                                 df1.index[16]: '#c61e00',
                                                 df1.index[17]: '#bd0000'})
-
+            # make the First Watch line thicker
             fig1.update_traces(line=dict(width=4), selector=dict(name='First Watch'))                                     
             fig1.update_yaxes(title_text='')
 
@@ -124,28 +122,31 @@ for i in range(len(tabs)):
         
             # add smaller title before the charts
             st.write(f'#### {"Weekly YoY " + title +" Growth"}')
-            st.dataframe(styled_df2, height=(df2_with_blanks.shape[0] + 1) * 35 + 3)
+            # display the YoY heatmap df, set the height
+            st.dataframe(styled_df2, height=(df2_with_blanks.shape[0] + 1) * 35 + 8)
             
+            # display the averages in a dropdown
             with st.expander('Averages'):
                 st.dataframe(styled_dfAverages)
             
+            # plot the line charts
             st.plotly_chart(fig2)
             st.write(f'#### {"Weekly " + title}')
             st.plotly_chart(fig1)
             
-            
-            # display the two dfs
-            with st.expander(title):
+            # display the full data in a dropdown
+            with st.expander(f'{"Weekly " + title}'):
                 st.write(df1)        
-                
+            
+            # site source of data
             if i <= 2:
                 st.write('Data from Consumer Edge, a panel of credit card transactions')       
             else:
                 st.write('Data from Placer.ai, a panel of mobile phone location data') 
                 
         else:
+            # for "Number of Orders by Income" tab, repeat data cleaning but with visualizations 
             title = tabs[i]
-            # import data output.xslx
             df = pd.read_excel('./output.xlsx', sheet_name=title)
 
             blankIndex = df[df.isnull().all(1)].index[0]
@@ -154,7 +155,6 @@ for i in range(len(tabs)):
             dfYoY = pd.read_excel('./output.xlsx', sheet_name=title, skiprows= lambda x: x not in range(blankIndex+2, df.shape[0]+1))
 
 
-             # change anything datetime to string including headers 
             for col in range(len(dfYoY.columns)):
                 if isinstance(dfYoY.columns[col], datetime.date):
                     dfYoY = dfYoY.rename(columns={dfYoY.columns[col]: dfYoY.columns[col].strftime('%m/%d/%Y')})
@@ -162,24 +162,23 @@ for i in range(len(tabs)):
                     to_drop = col
                     break
 
-            # drop columns from to_drop onwards 
             dfYoY = dfYoY.iloc[:, :to_drop]
             
-            # category column = FW for first 7 rows, then Breakfast, then Casual Dining
+            # add category column: FW for first 7 rows (7 income groups), then Breakfast, then Casual Dining
             dfYoY['Category'] =  ['FW']*7 + ['Breakfast']*7 + ['Casual Dining']*7
             # index is category and Income
             dfYoY.set_index(['Category', 'Income'], inplace=True)
             
-            # add blank row to row 2 and move everything down
+            # add blank rows to separate groups
             dfYoY_blank_bc = pd.DataFrame(np.nan, index=[("BREAKFAST","-")], columns=dfYoY.columns)
             dfYoY_blank_cd = pd.DataFrame(np.nan, index=[("CASUAL DINING","-")], columns=dfYoY.columns)
             dfYOY_with_blanks = pd.concat([dfYoY.iloc[:7], dfYoY_blank_bc, dfYoY.iloc[7:14], dfYoY_blank_cd, dfYoY.iloc[14:]])
-            
-            styled_dfYoY = dfYOY_with_blanks.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
+                        
+            divnorm = colors.TwoSlopeNorm(vmin=min(dfYoY.values.min(),-.001), vcenter=0, vmax=max(dfYoY.values.max(),.001))
+            styled_dfYoY = dfYOY_with_blanks.style.background_gradient(cmap="RdYlGn", axis=None, vmin=0, vmax=1, gmap=dfYoY.apply(divnorm)).format("{:.0%}")
             
             st.write(f'#### {"Weekly YoY Order Volume Growth by Income"}')
-            
-            st.dataframe(styled_dfYoY, height=(df2_with_blanks.shape[0] + 1) * 35 + 3)
+            st.dataframe(styled_dfYoY, height=(dfYOY_with_blanks.shape[0] + 1) * 35 + 8)
             st.write('Data from Consumer Edge, a panel of credit card transactions')  
             
 # add a 'Raw Data" tab where you can download the data
