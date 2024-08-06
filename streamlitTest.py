a2dfdf50c8dea7b80b431f7f92b6a74c364fe28e
Zachary Wayne <zachrwayne@gmail.com>
2024-08-06 09:12:40 -0400

    updated from comments

--- streamlitTest.py
+++ streamlitTest.py
@@ -7,9 +7,6 @@ import seaborn as sns
 import os
 import plotly.express as px
 
-# def run_script():
-#     os.system('python Self_Service_data_pull.py')
-
 st.set_page_config(layout="wide")
 
 st.title('First Watch Report')
@@ -21,20 +18,13 @@ blankIndex = df[df.isnull().all(1)].index[0]
 
 df1 = pd.read_excel('output.xlsx', skiprows= lambda x: x not in range(blankIndex+1))
 
-# if st.button('Update Data'):
-#     run_script()
-    
-#get unique brands as a selectbox
-brands = df1['Unnamed: 1'].unique()
-brand = st.multiselect('Select Brand', brands)
+# Insert containers separated into tabs:
+tabs = ["Total Revenue", "AOV", "Number of Transactions", "Visits", "Number of Orders by Income"]
+tabslist = st.tabs(tabs + ['Download Raw Data'])    
 
-if st.button('Get Analysis'):
-    # Insert containers separated into tabs:
-    tabs = ["Total Revenue", "AOV", "Number of Transactions", "Visits", "Panel Visits"]
-    tabslist = st.tabs(tabs)    
-
-    for i in range(len(tabs)):
-        with tabslist[i]:
+for i in range(len(tabs)):
+    with tabslist[i]:
+        if i != len(tabs)-1:
             title = tabs[i]
             # import data output.xslx
             df = pd.read_excel('output.xlsx', sheet_name=title)
@@ -52,11 +42,6 @@ if st.button('Get Analysis'):
             # remove category column
             df1 = df1.iloc[:, 1:]
             df2 = df2.iloc[:, 1:]
-            
-            # drop rows where Brand Name is not in brand
-            if len(brand) > 0:
-                df1 = df1[df1.index.get_level_values('Brand Name').isin(brand)]
-                df2 = df2[df2.index.get_level_values('Brand Name').isin(brand)]
 
             # index = first two columns
             to_drop = None
@@ -71,29 +56,137 @@ if st.button('Get Analysis'):
                     to_drop = col
                     break
 
-            # drop columns from to_drop onwards
+            # drop columns from to_drop onwards 
             df2 = df2.iloc[:, :to_drop]
             
-            styled_df2 = df2.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
+            # add blank row to row 2 and move everything down
+            df_blank_bc = pd.DataFrame(np.nan, index=["BREAKFAST COMPS"], columns=df2.columns)
+            df_blank_cd = pd.DataFrame(np.nan, index=["CASUAL DINING COMPS"], columns=df2.columns)
+            df2_with_blanks = pd.concat([df2.iloc[:1], df_blank_bc, df2.iloc[1:9], df_blank_cd, df2.iloc[9:]])
+            
+            styled_df2 = df2_with_blanks.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
+            
+            # create df of just the first row, then the average of rows 2-9, then the average of rows 10-17
+            dfAverages = pd.concat([df2_with_blanks.iloc[0], df2_with_blanks.iloc[2:10].mean(), df2_with_blanks.iloc[12:].mean()], axis=1).T
+            # index the averages
+            dfAverages.index = ['First Watch', 'Breakfast Average', 'Casual Dining Average']
+            styled_dfAverages = dfAverages.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
             
             # remove category column
             df1 = df1.iloc[:, 1:]
             df2 = df2.iloc[:, 1:]
         
             # making the charts
-            fig1 = px.line(df1.T)
+            fig1 = px.line(df1.T,
+                        color_discrete_map={'First Watch': 'black',
+                                                df1.index[1]: '#260279',
+                                                df1.index[2]: '#31029c',
+                                                df1.index[3]: '#1c0159',
+                                                df1.index[4]: '#22016d',
+                                                df1.index[5]: '#b697ff',
+                                                df1.index[6]: '#d3c0ff',
+                                                df1.index[7]: '#9362ff',
+                                                df1.index[8]: '#a881ff',
+                                                df1.index[9]: '#ffea00',
+                                                df1.index[10]: '#f7cd00',
+                                                df1.index[11]: '#efb000',
+                                                df1.index[12]: '#e79300',
+                                                df1.index[13]: '#de7500',
+                                                df1.index[14]: '#d65800',
+                                                df1.index[15]: '#ce3b00',
+                                                df1.index[16]: '#c61e00',
+                                                df1.index[17]: '#bd0000'})
+
+            fig1.update_traces(line=dict(width=4), selector=dict(name='First Watch'))                                     
             fig1.update_yaxes(title_text='')
 
-            fig2 = px.line(df2.T)
+            fig2 = px.line(df2.T, 
+                        color_discrete_map={'First Watch': 'black',
+                                                df1.index[1]: '#260279',
+                                                df1.index[2]: '#31029c',
+                                                df1.index[3]: '#1c0159',
+                                                df1.index[4]: '#22016d',
+                                                df1.index[5]: '#b697ff',
+                                                df1.index[6]: '#d3c0ff',
+                                                df1.index[7]: '#9362ff',
+                                                df1.index[8]: '#a881ff',
+                                                df1.index[9]: '#ffea00',
+                                                df1.index[10]: '#f7cd00',
+                                                df1.index[11]: '#efb000',
+                                                df1.index[12]: '#e79300',
+                                                df1.index[13]: '#de7500',
+                                                df1.index[14]: '#d65800',
+                                                df1.index[15]: '#ce3b00',
+                                                df1.index[16]: '#c61e00',
+                                                df1.index[17]: '#bd0000'})
+            fig2.update_traces(line=dict(width=4), selector=dict(name='First Watch'))
             fig2.update_yaxes(title_text='')
         
-            st.plotly_chart(fig1)
+            # add smaller title before the charts
+            st.write(f'#### {"Weekly YoY " + title +" Growth"}')
+            st.dataframe(styled_df2, height=(df2_with_blanks.shape[0] + 1) * 35 + 3)
+            
+            with st.expander('Averages'):
+                st.dataframe(styled_dfAverages)
+            
             st.plotly_chart(fig2)
+            st.write(f'#### {"Weekly " + title}')
+            st.plotly_chart(fig1)
+            
             
             # display the two dfs
             with st.expander(title):
-                st.write(df1)
+                st.write(df1)        
+                
+            if i <= 2:
+                st.write('Data from Consumer Edge, a panel of credit card transactions')       
+            else:
+                st.write('Data from Placer.ai, a panel of mobile phone location data') 
+                
+        else:
+            title = tabs[i]
+            # import data output.xslx
+            df = pd.read_excel('./output.xlsx', sheet_name=title)
 
-            with st.expander('Full Data - Yoy Growth %'):
-                st.dataframe(styled_df2)
+            blankIndex = df[df.isnull().all(1)].index[0]
 
+            df1 = pd.read_excel('./output.xlsx', sheet_name=title, skiprows= lambda x: x not in range(blankIndex+1))
+            dfYoY = pd.read_excel('./output.xlsx', sheet_name=title, skiprows= lambda x: x not in range(blankIndex+2, df.shape[0]+1))
+
+
+             # change anything datetime to string including headers 
+            for col in range(len(dfYoY.columns)):
+                if isinstance(dfYoY.columns[col], datetime.date):
+                    dfYoY = dfYoY.rename(columns={dfYoY.columns[col]: dfYoY.columns[col].strftime('%m/%d/%Y')})
+                elif 'Unnamed' in dfYoY.columns[col]: 
+                    to_drop = col
+                    break
+
+            # drop columns from to_drop onwards 
+            dfYoY = dfYoY.iloc[:, :to_drop]
+            
+            # category column = FW for first 7 rows, then Breakfast, then Casual Dining
+            dfYoY['Category'] =  ['FW']*7 + ['Breakfast']*7 + ['Casual Dining']*7
+            # index is category and Income
+            dfYoY.set_index(['Category', 'Income'], inplace=True)
+            
+            # add blank row to row 2 and move everything down
+            dfYoY_blank_bc = pd.DataFrame(np.nan, index=[("BREAKFAST","-")], columns=dfYoY.columns)
+            dfYoY_blank_cd = pd.DataFrame(np.nan, index=[("CASUAL DINING","-")], columns=dfYoY.columns)
+            dfYOY_with_blanks = pd.concat([dfYoY.iloc[:7], dfYoY_blank_bc, dfYoY.iloc[7:14], dfYoY_blank_cd, dfYoY.iloc[14:]])
+            
+            styled_dfYoY = dfYOY_with_blanks.style.background_gradient(cmap='RdYlGn', axis=None).format("{:.0%}")
+            
+            st.write(f'#### {"Weekly YoY Order Volume Growth by Income"}')
+            
+            st.dataframe(styled_dfYoY, height=(df2_with_blanks.shape[0] + 1) * 35 + 3)
+            st.write('Data from Consumer Edge, a panel of credit card transactions')  
+            
+# add a 'Raw Data" tab where you can download the data
+with tabslist[-1]:
+    st.write('#### Download Raw Data')
+    with open('./output.xlsx', 'rb') as f:
+        btn = st.download_button(
+             label ='Download',
+             data = f, 
+             file_name = 'output.xlsx',)
